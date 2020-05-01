f2eac26953b356eeb011addfaaa9b30f2532b331
Aryan Pegwar <rkt10952@gmail.com>
2020-05-02 00:02:00 +0530

    Re-arranged code

--- tkinter_app.py
+++ tkinter_app.py
@@ -3,11 +3,12 @@ from tkinter import *
 master = Tk()
 master.title('Rock Paper Scissor')
 master.geometry('300x300')
+
 heading_frame = Frame(master)
-text = Text(heading_frame)
-text.insert(INSERT, "Welcome to rock paper scissor")
 heading_frame.pack()
 
+text = Text(heading_frame)
+text.insert(INSERT, "Welcome to rock paper scissor")
 text.pack()
 
 mainloop()
