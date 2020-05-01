5dbb0cf9f0fc8d74c5ab768408df30a925d5b2fc
Aryan Pegwar <rkt10952@gmail.com>
2020-05-02 00:05:58 +0530

    Change font of heaading

--- tkinter_app.py
+++ tkinter_app.py
@@ -2,12 +2,12 @@ from tkinter import *
 
 master = Tk()
 master.title('Rock Paper Scissor')
-master.geometry('300x300')
+master.geometry('600x600')
 
 heading_frame = Frame(master)
 heading_frame.pack()
 
-text = Text(heading_frame)
+text = Text(heading_frame, font=('Comic Sans MS', 30))
 text.insert(INSERT, "Welcome to rock paper scissor")
 text.pack()
 
