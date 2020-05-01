e27682b4fb57685c7e7ad2b89cb1da6a95d9c894
Aryan Pegwar <rkt10952@gmail.com>
2020-05-02 00:29:11 +0530

    Widgits arrange

--- tkinter_app.py
+++ tkinter_app.py
@@ -5,15 +5,16 @@ master.title('Rock Paper Scissor')
 master.geometry('600x600')
 
 heading_frame = Frame(master)
-heading_frame.pack()
+heading_frame.pack(side=TOP)
 
 text = Label(heading_frame, text="Welcome to rock paper scissor", font=('Comic Sans MS', 30))
 text.pack()
 
 username_frame = Frame(master)
 L1 = Label(username_frame, text="User Name")
-L1.pack(side=BOTTOM)
+L1.pack(side=LEFT)
 E1 = Entry(username_frame, bd=5)
-E1.pack(side=BOTTOM)
+E1.pack(side=LEFT)
 username_frame.pack(side=LEFT)
+
 mainloop()
